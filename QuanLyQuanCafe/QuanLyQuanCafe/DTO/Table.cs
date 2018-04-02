34bdbf46a6eac6ce29a534f46901bf27981a868c
diepdandy <34849727+diepdandy@users.noreply.github.com>
2018-04-02 11:35:59 +0700

    DTO 1

---
+++ QuanLyQuanCafe/QuanLyQuanCafe/DTO/Table.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeManager.Controllers
+{
+    public class Table
+    {
+        private string iD;
+        private string tenBan;
+        private string khuVuc;
+        private string trangThai;
+
+        public Table(string iD, string tenBan, string khuVuc, string trangThai)
+        {
+            this.ID = iD;
+            this.TenBan = tenBan;
+            this.KhuVuc = khuVuc;
+            this.TrangThai = trangThai;
+        }
+        public Table(DataRow row)
+        {
+            this.ID = row["maBan"].ToString();
+            this.TenBan = row["tenBan"].ToString();
+            this. KhuVuc = row["khuVuc"].ToString();
+            this.TrangThai = row["trangThai"].ToString();
+        }
+        public string ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+
+        public string TenBan
+        {
+            get { return tenBan; }
+            set { tenBan = value; }
+        }
+
+        public string KhuVuc
+        {
+            get { return khuVuc; }
+            set { khuVuc = value; }
+        }
+
+        public string TrangThai
+        {
+            get { return trangThai; }
+            set { trangThai = value; }
+        }
+
+        public string Name { get; internal set; }
+    }
+}
